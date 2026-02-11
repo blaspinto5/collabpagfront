9422b639a69c457ede738b66f37b00b9e2e8d492
Camila F <blaspinto.2000@gmail.com>
2026-02-11 18:13:10 -0300

    front

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -10,8 +10,10 @@ const MainLayout = () => {
   return (
     <div className="app-background min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 px-6 md:px-10 lg:px-16">
-        <Outlet />
+      <main className="flex-1 w-full flex justify-center">
+        <div className="w-full max-w-7xl px-6 md:px-10 lg:px-16">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
